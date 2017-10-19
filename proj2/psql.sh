af1b34f6acbdb54679fdac0975eeb387a02bab7d
MrinaliniS <shirimrinalini@gmail.com>
2017-10-19 17:18:48 +0530

    Update psql.sh

--- proj2/psql.sh
+++ proj2/psql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sh /usr/local/greenplum-db/greenplum_path.sh
-sh /usr/local/greenplum-db/bin/gpstate
+. /usr/local/greenplum-db/greenplum_path.sh
+. /usr/local/greenplum-db/bin/gpstate
 
 #gpstart -d /home
 #psql -c "select current_date" 
