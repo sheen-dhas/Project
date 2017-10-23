ce3c1835dd169d30298ce23f82cc4105212d0cdb
MrinaliniS <shirimrinalini@gmail.com>
2017-10-23 13:56:09 +0530

    Update psql.sh

--- proj2/psql.sh
+++ proj2/psql.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 . /usr/local/greenplum-db/greenplum_path.sh
-su - gpadmin - pivotal "/usr/local/greenplum-db/bin/gpstart -d /gpdata/segments/gpseg1"
-
+sudo - gpadmin /usr/local/greenplum-db/bin/gpstart -d "/gpdata/segments/gpseg1"
 
 ##ls /*
 
