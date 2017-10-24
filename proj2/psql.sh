86c905cb117d7eef6a6267ab3c0ebcbead57a66e
MrinaliniS <shirimrinalini@gmail.com>
2017-10-24 16:34:40 +0530

    Update psql.sh

--- proj2/psql.sh
+++ proj2/psql.sh
@@ -19,11 +19,7 @@ su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "CREATE schema trial" '
 
 su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "CREATE TABLE trial.weather (city integer, name text)" '
 
-su - postgres -c "ans='aaa'"
-
-su - postgres -c "echo $ans"
-
-su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "insert into trial.weather (city,name) values (1,"aaa")" '
+su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "insert into trial.weather (city,name) values (1,"'"aaa"'")" '
 
 su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "select * from trial.weather" '
 
