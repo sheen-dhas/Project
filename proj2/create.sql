f61afff3c26d0157da4b41041c36d82e58ec6ded
MrinaliniS <shirimrinalini@gmail.com>
2017-10-24 16:45:24 +0530

    Update create.sql

--- proj2/create.sql
+++ proj2/create.sql
@@ -1,11 +1,2 @@
-create database test;
-
-create table test.trial
-(
-id integer,
-name character varying(20)
-)
-WITH (APPENDONLY=true, COMPRESSLEVEL=5, ORIENTATION=row, COMPRESSTYPE=zlib, 
-  OIDS=FALSE
-)
-DISTRIBUTED BY (id);
+CREATE schema trial;
+CREATE TABLE trial.weather (city integer, name integer);
