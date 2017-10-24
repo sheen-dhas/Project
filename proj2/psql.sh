c118b3d02bbfeea80a0809b4e618997509dd95c6
MrinaliniS <shirimrinalini@gmail.com>
2017-10-24 12:26:24 +0530

    Update psql.sh

--- proj2/psql.sh
+++ proj2/psql.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-. /usr/local/greenplum-db/greenplum_path.sh
-/usr/local/greenplum-db/bin/gpstart -d "/gpdata/segments/gpseg1"
+
+ls /*
+
+##. /usr/local/greenplum-db/greenplum_path.sh
+##/usr/local/greenplum-db/bin/gpstart -d "/gpdata/segments/gpseg1"
 ##/usr/local/greenplum-db/bin/gpstart-R -d "/gpdata/segments/gpseg1"
 
 ##ls /*
