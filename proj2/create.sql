df68c1d99ef9416db52613909765c8a5c683ceb6
MrinaliniS <shirimrinalini@gmail.com>
2017-10-24 17:04:39 +0530

    Update create.sql

--- proj2/create.sql
+++ proj2/create.sql
@@ -1,2 +1,4 @@
+CREATE schema trial;
 CREATE TABLE trial.weather (city integer, name varchar(10));
 insert into trial.weather (city,name) values (1,'aaa');
+insert into trial.weather (city,name) values (2,'a1');
