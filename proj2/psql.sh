e0d043e4ef2f99e461933f9dadda9315156c2e38
MrinaliniS <shirimrinalini@gmail.com>
2017-10-24 16:14:10 +0530

    Update psql.sh

--- proj2/psql.sh
+++ proj2/psql.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-##ls /*
-
-##ls /run/*
-##ls /etc/*
-##ls /var/*
-
-##. /run/postgresql "select * from weather"
 
 cd /home
 mkdir postgres
@@ -31,34 +24,3 @@ su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "insert into trial.weather 
 su - postgres -c '/usr/lib/postgresql/10/bin/psql -c "select * from trial.weather" '
 
 exit;
-##ls
-
-##pg_ctl start
-
-##./postgres 
-##su -c 'su postgres'
-##pg_ctl start -f a.out
-##createdb mydb
-
-##ls /run/*
-
-##cd /usr/bin/
-##psql -c "\d"
-
-
-##mydb
-
-##. /run/postgresql "CREATE TABLE weather (city varchar(80))"
-
-##. /usr/local/greenplum-db/greenplum_path.sh
-##/usr/local/greenplum-db/bin/gpstart -d "/gpdata/segments/gpseg1"
-##/usr/local/greenplum-db/bin/gpstart-R -d "/gpdata/segments/gpseg1"
-
-##ls /*
-
-#ls /gpdata/master/*
-#ls /gpdata/segments/*
-#/data/master/
-
-#gpstart -d /home
-#psql -c "select current_date" 
