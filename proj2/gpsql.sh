4f80310a55edce09006371433222752f48fbe836
MrinaliniS <shirimrinalini@gmail.com>
2017-10-25 15:04:57 +0530

    Update gpsql.sh

--- proj2/gpsql.sh
+++ proj2/gpsql.sh
@@ -5,14 +5,13 @@ cd /usr/lib/apt/methods
 
 ls
 
-./ssh gpadmin@10.63.33.203 
-
-##<<!
-##gpadmin
-##psql -c "CREATE schema trial";
-##psql -c "CREATE TABLE trial.weather (city integer, name varchar(10))";
-##psql -c "insert into trial.weather (city,name) values (1,'aaa')";
-##psql -c "insert into trial.weather (city,name) values (2,'a1')";
-##!
+ssh gpadmin@10.63.33.203 
+<<!
+gpadmin
+psql -c "CREATE schema trial";
+psql -c "CREATE TABLE trial.weather (city integer, name varchar(10))";
+psql -c "insert into trial.weather (city,name) values (1,'aaa')";
+psql -c "insert into trial.weather (city,name) values (2,'a1')";
+!
 
 
