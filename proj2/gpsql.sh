e10aa7407c0a642ff9ee23647a4a5b08aee435e6
MrinaliniS <shirimrinalini@gmail.com>
2017-10-25 17:10:08 +0530

    Update gpsql.sh

--- proj2/gpsql.sh
+++ proj2/gpsql.sh
@@ -1,13 +1,16 @@
 
 #!/bin/sh
-
+#!/usr/bin/expect -f
 ##apt-get install ssh
 
 apk update
 
 apk add openssh
 
-echo gpadmin |ssh -o "StrictHostKeyChecking no" gpadmin@10.63.33.203
+spawn ssh -o "StrictHostKeyChecking no" gpadmin@10.63.33.203
+expect "assword:"
+send "gpadmin\r"
+interact
 psql -c "CREATE schema trial"
 
 
