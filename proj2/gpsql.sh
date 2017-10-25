b5f2ab0b47246df91fe0984325fd3f8fdf5c1ae4
MrinaliniS <shirimrinalini@gmail.com>
2017-10-25 15:07:46 +0530

    Update gpsql.sh

--- proj2/gpsql.sh
+++ proj2/gpsql.sh
@@ -8,10 +8,10 @@ ls
 . /usr/lib/apt/methods/ssh gpadmin@10.63.33.203 
 <<!
 gpadmin
-psql -c "CREATE schema trial";
-psql -c "CREATE TABLE trial.weather (city integer, name varchar(10))";
-psql -c "insert into trial.weather (city,name) values (1,'aaa')";
-psql -c "insert into trial.weather (city,name) values (2,'a1')";
+psql -c "CREATE schema trial"
+psql -c "CREATE TABLE trial.weather (city integer, name varchar(10))"
+psql -c "insert into trial.weather (city,name) values (1,'aaa')"
+psql -c "insert into trial.weather (city,name) values (2,'a1')"
 !
 
 
