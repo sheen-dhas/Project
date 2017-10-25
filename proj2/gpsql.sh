910e92ef025f3b72f02860a33d810951c0471244
MrinaliniS <shirimrinalini@gmail.com>
2017-10-25 17:17:44 +0530

    Update gpsql.sh

--- proj2/gpsql.sh
+++ proj2/gpsql.sh
@@ -11,7 +11,7 @@ apk add sshpass
 
 sshpass -p 'gpadmin' ssh -o "StrictHostKeyChecking no" gpadmin@10.63.33.203 <<!
 psql -c "CREATE schema trial"
-psql -c "CREATE TABLE trial.weather (city integer, name varchar(10))"
+psql -c "CREATE TABLE trial.weather (city integer, name varchar(10)) DISTRIBUTED BY city"
 psql -c "insert into trial.weather (city,name) values (1,'aaa')"
 psql -c "insert into trial.weather (city,name) values (2,'a1')"
 !
