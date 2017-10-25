0f15a45958b005f2d461ebe7bad73b22dd33ccc0
MrinaliniS <shirimrinalini@gmail.com>
2017-10-25 16:44:05 +0530

    Update gpsql.sh

--- proj2/gpsql.sh
+++ proj2/gpsql.sh
@@ -9,8 +9,8 @@ apk add openssh
 
 ssh gpadmin@10.63.33.203 
 <<!
-yes
 gpadmin
+yes
 psql -c "CREATE schema trial"
 ##psql -c "CREATE TABLE trial.weather (city integer, name varchar(10))"
 ##psql -c "insert into trial.weather (city,name) values (1,'aaa')"
