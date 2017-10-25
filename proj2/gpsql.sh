c7ce17f84023b269bcb65b85ae90aa74b852fb9b
MrinaliniS <shirimrinalini@gmail.com>
2017-10-25 17:05:54 +0530

    Update gpsql.sh

--- proj2/gpsql.sh
+++ proj2/gpsql.sh
@@ -7,12 +7,10 @@ apk update
 
 apk add openssh
 
-ssh -o "StrictHostKeyChecking no" gpadmin@10.63.33.203<<!
-gpadmin
+ssh -o "StrictHostKeyChecking no" gpadmin@10.63.33.203
+expect "assword:"
+send "gpadmin\r"
+interact
 psql -c "CREATE schema trial"
-##psql -c "CREATE TABLE trial.weather (city integer, name varchar(10))"
-##psql -c "insert into trial.weather (city,name) values (1,'aaa')"
-##psql -c "insert into trial.weather (city,name) values (2,'a1')"
-!
 
 
