84b01278b38cf6d7810a60fd70dce8c207cbc2f0
MrinaliniS <shirimrinalini@gmail.com>
2017-10-25 15:00:25 +0530

    Update gpsql.sh

--- proj2/gpsql.sh
+++ proj2/gpsql.sh
@@ -7,7 +7,10 @@ ls
 
 ./ssh gpadmin@10.63.33.203 <<!
 gpadmin
-psql -c "SELECT datname FROM pg_database WHERE datistemplate = false"
+psql -c "CREATE schema trial";
+psql -c "CREATE TABLE trial.weather (city integer, name varchar(10))";
+psql -c "insert into trial.weather (city,name) values (1,'aaa')";
+psql -c "insert into trial.weather (city,name) values (2,'a1')";
 !
 
 
