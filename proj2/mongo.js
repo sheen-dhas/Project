de67de4a2c716adfd3306b9bc8d6869c43a0bbe6
MrinaliniS <shirimrinalini@gmail.com>
2017-10-25 14:22:14 +0530

    Update mongo.js

--- proj2/mongo.js
+++ proj2/mongo.js
@@ -1,14 +1,5 @@
-var MongoClient = require('mongodb').MongoClient;
-//Create a database named "mydb":
-var url = "mongodb://localhost:27017/db";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
-
+use db;
 
 db.createCollection("emp");
 db.emp.insert({id:1});
-db.getCollection("emp").find(
+db.getCollection("emp").find()
