7506816c66014ef9f816256accebf88b12784680
MrinaliniS <shirimrinalini@gmail.com>
2017-10-30 15:25:09 +0530

    Update run_h.sh

--- proj2/run_h.sh
+++ proj2/run_h.sh
@@ -2,6 +2,8 @@
 
 export PYSPARK_PYTHON=/spark-1.6.2-bin-hadoop2.6/python
 
+chmod 777 /spark-1.6.2-bin-hadoop2.6/python/*
+
 cd /spark-1.6.2-bin-hadoop2.6/bin
 
 ./spark-submit /spark-1.6.2-bin-hadoop2.6/examples/src/main/python/wordcount.py
