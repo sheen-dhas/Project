2696b6eeb111fd8529d140d20d9138e15843e6c0
MrinaliniS <shirimrinalini@gmail.com>
2017-11-02 12:17:01 +0530

    Create run.py

---
+++ proj2/run.py
@@ -0,0 +1,21 @@
+import os
+from pyspark import SparkConf,SparkContext
+from pyspark.sql import SQLContext,HiveContext
+
+from pyspark.sql.functions import udf
+from pyspark.sql.types import *
+
+import sys
+
+conf = (SparkConf()
+  .setAppName("data_import")
+  .set("spark.shuffle.service.enabled","true"))
+
+sc = SparkContext(conf = conf)
+ 
+sqlContext= SQLContext(sc)
+
+data = [1, 2, 3, 4, 5]
+distData = sc.parallelize(data)
+
+distData.show()
