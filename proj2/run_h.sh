edc942560749a28f3d6ce8b2a2fd2123a9944bc0
MrinaliniS <shirimrinalini@gmail.com>
2017-11-07 14:56:11 +0530

    Update run_h.sh

--- proj2/run_h.sh
+++ proj2/run_h.sh
@@ -9,10 +9,10 @@ chmod 777 trial.csv
 
 ##ls /home/build-spark/*
 
-ls /home/build-spark/rootfs/usr/java/jdk1.8.0_131/*
+ls /home/build-spark/rootfs/usr/java/*
 
-export JAVA_HOME=/home/build-spark/rootfs/usr/java/jdk1.8.0_131
-export PATH=$PATH:/home/build-spark/rootfs/usr/java/jdk1.8.0_131/bin
+##export JAVA_HOME=/home/build-spark/rootfs/usr/java/jdk1.8.0_131
+##export PATH=$PATH:/home/build-spark/rootfs/usr/java/jdk1.8.0_131/bin
 
 ls /home/build-spark/rootfs/*
 
