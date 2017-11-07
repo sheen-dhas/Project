3c538997282a9fd84b22c1f44e5d721e8ce00c95
MrinaliniS <shirimrinalini@gmail.com>
2017-11-07 15:36:07 +0530

    Create spark-env.sh

---
+++ proj2/spark-env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set HADOOP_HOME=/home/build-spark/rootfs/usr/hadoop-2.7.3
+set PATH=$PATH:/home/build-spark/rootfs/usr/hadoop-2.7.3/bin
+set SPARK_DIST_CLASSPATH=/home/build-spark/rootfs/usr/hadoop-2.7.3/bin/hadoop
