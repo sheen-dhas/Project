10892c31cc79c93a4137893d09c2b93c40f836cd
MrinaliniS <shirimrinalini@gmail.com>
2017-11-07 15:39:13 +0530

    Update spark-env.sh

--- proj2/spark-env.sh
+++ proj2/spark-env.sh
@@ -2,4 +2,3 @@
 
 set HADOOP_HOME=/home/build-spark/rootfs/usr/hadoop-2.7.3
 set PATH=$PATH:/home/build-spark/rootfs/usr/hadoop-2.7.3/bin
-set SPARK_DIST_CLASSPATH=/home/build-spark/rootfs/usr/hadoop-2.7.3/bin/hadoop
