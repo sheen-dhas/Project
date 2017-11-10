e98bbd186ce4bcbf1b6be3daa99c5d079c7bb2c4
MrinaliniS <shirimrinalini@gmail.com>
2017-11-10 15:11:54 +0530

    Update run.py

--- proj2/run.py
+++ proj2/run.py
@@ -18,6 +18,9 @@ sqlContext= SQLContext(sc)
 cat_main = json.dumps({"table":{"namespace":"default", "name":"employee", "tablecoder":"primitivetype"},"rowkey":"rowkey","columns":{"rowkey":{"cf":"rowkey", "col":"rowkey","type":"string"},\
 "id":{"cf":"ericcson","col":"id","type":"string"}}})
 
+cat = json.dumps({"table":{"namespace":"default", "name":"employee1", "tablecoder":"primitivetype"},"rowkey":"rowkey","columns":{"rowkey":{"cf":"rowkey", "col":"rowkey","type":"string"},\
+"id":{"cf":"ericcson","col":"id","type":"string"}}})
+
 df=sqlContext.read.option("catalog",cat_main).option("newtable","2").format("org.apache.spark.sql.execution.datasources.hbase").load()
 
 df.registerTempTable("tim_ericcson_bulk")
@@ -25,3 +28,5 @@ df.registerTempTable("tim_ericcson_bulk")
 aa=sqlContext.sql("select * from tim_ericcson_bulk")
 
 aa.show()
+
+aa.write.option("catalog",cat).option("newtable","2").format("org.apache.spark.sql.execution.datasources.hbase").save()
