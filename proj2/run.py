6bcfc16f0ebada77e7fa937492b5ab136c41809e
MrinaliniS <shirimrinalini@gmail.com>
2017-11-10 11:54:17 +0530

    Update run.py

--- proj2/run.py
+++ proj2/run.py
@@ -1,23 +1,31 @@
-from pyspark.sql import SparkSession
+import os
+from pyspark import SparkConf,SparkContext
+from pyspark.sql import SQLContext,HiveContext
 
-spark = SparkSession \
-    .builder \
-    .appName("Python Spark SQL basic example") \
-    .config("spark.some.config.option", "some-value") \
-    .getOrCreate()
+from pyspark.sql.functions import udf
+from pyspark.sql.types import *
 
 
 import json
 
 import sys
 
+conf = (SparkConf()
+  .setAppName("data_import")
+  .set("spark.shuffle.service.enabled","true"))
+  
+
+sc = SparkContext(conf = conf)
+
+sqlContext= SQLContext(sc)
+
 cat_main = json.dumps({"table":{"namespace":"default", "name":"employee", "tablecoder":"primitivetype"},"rowkey":"rowkey","columns":{"rowkey":{"cf":"rowkey", "col":"rowkey","type":"string"},\
 "id":{"cf":"ericcson","col":"id","type":"string"}}})
 
-df=spark.read.option("catalog",cat_main).option("newtable","2").format("org.apache.spark.sql.execution.datasources.hbase").load()
+df=sqlContext.read.option("catalog",cat_main).option("newtable","2").format("org.apache.spark.sql.execution.datasources.hbase").load()
 
 df.registerTempTable("tim_ericcson_bulk")
 
-aa=spark.sql("select * from tim_ericcson_bulk")
+aa=sqlContext.sql("select * from tim_ericcson_bulk")
 
 aa.show()
