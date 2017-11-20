868f8fbcd0d61b255683e7e06b9dc5747f2dd8d2
MrinaliniS <shirimrinalini@gmail.com>
2017-11-20 15:30:25 +0530

    Update deploy.sh

--- proj2/deploy.sh
+++ proj2/deploy.sh
@@ -1,7 +1,37 @@
+
 #!/bin/sh
+#!/usr/bin/expect -f
+##apt-get install ssh
 
-hbase shell ./sample_commands.txt
+ls
 
-spark-submit  --packages com.hortonworks:shc-core:1.1.1-2.1-s_2.11 --repositories http://repo.hortonworks.com/content/groups/public/ run.py
+cp run.py /home
+chmod 777 run.py
 
-hbase shell ./list.txt
+cp run.sh /home
+chmod 777 run.sh
+
+cp sample_commands.txt /home
+chmod 777 sample_commands.txt
+
+cp list.txt /home
+chmod 777 list.txt
+
+apk update
+
+apk add openssh
+
+apk add sshpass
+
+apk add scp
+
+sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" run.py hduser1@10.63.33.207:/home/hduser1
+sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" run.sh hduser1@10.63.33.207:/home/hduser1
+sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" sample_commands.txt hduser1@10.63.33.207:/home/hduser1
+sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" list.txt hduser1@10.63.33.207:/home/hduser1
+
+
+sshpass -p 'Welcome@321' ssh -o "StrictHostKeyChecking no" hduser1@10.63.33.207 <<!
+ls /home/hduser1/*
+. /home/hduser1/run.sh
+!
