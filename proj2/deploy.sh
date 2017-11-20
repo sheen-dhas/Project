3010b21910622b966a5a48361dcdb1042efdc5ef
MrinaliniS <shirimrinalini@gmail.com>
2017-11-20 16:01:03 +0530

    Update deploy.sh

--- proj2/deploy.sh
+++ proj2/deploy.sh
@@ -9,7 +9,7 @@ cp run.py /home
 chmod 777 run.py
 
 cp run.sh /home
-chmod 777 run.sh
+chmod 777 run_h.sh
 
 cp sample_commands.txt /home
 chmod 777 sample_commands.txt
@@ -26,12 +26,12 @@ apk add sshpass
 apk add scp
 
 sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" run.py hduser1@10.63.33.207:/home/hduser1
-sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" run.sh hduser1@10.63.33.207:/home/hduser1
+sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" run_h.sh hduser1@10.63.33.207:/home/hduser1
 sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" sample_commands.txt hduser1@10.63.33.207:/home/hduser1
 sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" list.txt hduser1@10.63.33.207:/home/hduser1
 
 
 sshpass -p 'Welcome@321' ssh -o "StrictHostKeyChecking no" hduser1@10.63.33.207 <<!
 ls /home/hduser1/*
-. /home/hduser1/run.sh
+. /home/hduser1/run_h.sh
 !
