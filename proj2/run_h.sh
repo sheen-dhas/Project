4fb0b5d40f1a173a2119f90191f40b31ce306e3e
MrinaliniS <shirimrinalini@gmail.com>
2017-11-22 16:39:18 +0530

    Update run_h.sh

--- proj2/run_h.sh
+++ proj2/run_h.sh
@@ -20,16 +20,16 @@ apt-get install -y openssh-server
 
 ##hbase shell ./list.txt
 
-##hadoop fs -mkdir /tmp/tim_hfile
+hadoop fs -mkdir /tmp/tim_hfile
 
-##hadoop fs -mkdir /tmp/tim_hfile/ericcson
+hadoop fs -mkdir /tmp/tim_hfile/ericcson
 
-#hadoop fs -copyFromLocal /tmp/build/df6ad190/resource-tutorial/proj2/646f8e796f044f1d8e459f2d28f4caab /tmp/tim_hfile/ericcson/
+hadoop fs -copyFromLocal /tmp/build/df6ad190/resource-tutorial/proj2/646f8e796f044f1d8e459f2d28f4caab /tmp/tim_hfile/ericcson/
 
-#hadoop fs -chmod -R 0777 /tmp/tim_hfile/ericcson/*
+hadoop fs -chmod -R 0777 /tmp/tim_hfile/ericcson/*
 
-#hadoop fs -ls /tmp/tim_hfile/ericcson/
+hadoop fs -ls /tmp/tim_hfile/ericcson/
 
-#hadoop fs -chmod 777 /tmp/tim_hfile/ericcson/646f8e796f044f1d8e459f2d28f4caab
+##hadoop fs -ls /tmp/tim_hfile/ericcson/646f8e796f044f1d8e459f2d28f4caab
 
-#HADOOP_CLASSPATH=`/opt/hbase/bin/hbase classpath` /usr/hadoop-2.7.3/bin/hadoop jar /opt/hbase/lib/hbase-server-1.2.4.jar completebulkload hdfs://6b696db2-817f-4f09-5746-d86d39ba907a/tmp/tim_hfile/  tim_ericcson_bulk
+HADOOP_CLASSPATH=`/opt/hbase/bin/hbase classpath` /usr/hadoop-2.7.3/bin/hadoop jar /opt/hbase/lib/hbase-server-1.2.4.jar completebulkload hdfs://127.0.0.1:8020/tmp/tim_hfile/  tim_ericcson_bulk
