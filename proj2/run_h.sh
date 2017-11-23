20ea9166aa6b3e60a2d57ecb53a7eda097bfdc3a
MrinaliniS <shirimrinalini@gmail.com>
2017-11-23 11:20:18 +0530

    Update run_h.sh

--- proj2/run_h.sh
+++ proj2/run_h.sh
@@ -19,7 +19,7 @@ apt-get install -y openssh-server
 ##rm /etc/hosts
 ##cp /home/hosts /etc/hosts
 
-hbase shell ./sample_commands.txt
+##hbase shell ./sample_commands.txt
 
 ##spark-submit  --packages com.hortonworks:shc-core:1.1.1-2.1-s_2.11 --repositories http://repo.hortonworks.com/content/groups/public/ run.py
 
@@ -35,16 +35,12 @@ hbase shell ./sample_commands.txt
 
 ##/usr/hadoop-2.7.3/sbin/start-all.sh
 
-##hadoop fs -mkdir /tmp/tim_hfile
+hadoop fs -mkdir /tmp/tim_hfile
 
-##hadoop fs -mkdir /tmp/tim_hfile/ericcson
+hadoop fs -mkdir /tmp/tim_hfile/ericcson
 
-##hadoop fs -copyFromLocal /tmp/build/df6ad190/resource-tutorial/proj2/646f8e796f044f1d8e459f2d28f4caab /tmp/tim_hfile/ericcson/
+hadoop fs -copyFromLocal /tmp/build/df6ad190/resource-tutorial/proj2/646f8e796f044f1d8e459f2d28f4caab /tmp/tim_hfile/ericcson/
 
-##hadoop fs -chmod -R 0777 /tmp/tim_hfile/ericcson/*
+hadoop fs -chmod -R 0777 /tmp/tim_hfile/ericcson/*
 
-##hadoop fs -ls /tmp/tim_hfile/ericcson/
-
-##hadoop fs -ls /tmp/tim_hfile/ericcson/646f8e796f044f1d8e459f2d28f4caab
-
-##HADOOP_CLASSPATH=`/opt/hbase/bin/hbase classpath` /usr/hadoop-2.7.3/bin/hadoop jar /opt/hbase/lib/hbase-server-1.2.4.jar completebulkload /tmp/tim_hfile/  tim_ericcson_bulk
+HADOOP_CLASSPATH=`/opt/hbase/bin/hbase classpath` /usr/hadoop-2.7.3/bin/hadoop jar /opt/hbase/lib/hbase-server-1.2.4.jar completebulkload /tmp/tim_hfile/  tim_ericcson_bulk
