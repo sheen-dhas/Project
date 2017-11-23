c6b69386515b5888c177161e8eb636122a5928b1
MrinaliniS <shirimrinalini@gmail.com>
2017-11-23 13:53:17 +0530

    Update deploy_h.sh

--- proj2/deploy_h.sh
+++ proj2/deploy_h.sh
@@ -10,6 +10,12 @@ chmod 777 run_hbase.sh
 cp list.txt /home
 chmod 777 list.txt
 
+cp hbase_commands.txt /home
+chmod 777 hbase_commands.txt
+
+cp 646f8e796f044f1d8e459f2d28f4caab /home
+chmod 777 646f8e796f044f1d8e459f2d28f4caab
+
 apk update
 
 apk add openssh
@@ -20,6 +26,8 @@ apk add scp
 
 sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" run_hbase.sh hduser1@10.63.33.207:/home/hduser1
 sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" list.txt hduser1@10.63.33.207:/home/hduser1
+sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" hbase_commands.txt hduser1@10.63.33.207:/home/hduser1
+sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" 646f8e796f044f1d8e459f2d28f4caab hduser1@10.63.33.207:/home/hduser1
 
 
 sshpass -p 'Welcome@321' ssh -o "StrictHostKeyChecking no" hduser1@10.63.33.207 <<!
