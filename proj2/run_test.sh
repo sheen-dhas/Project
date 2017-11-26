e14874c848e0f2c215e26bf004e8b81cdb42cd3f
sheen-dhas <31241211+sheen-dhas@users.noreply.github.com>
2017-11-26 19:48:29 +0530

    Update run_test.sh

--- proj2/run_test.sh
+++ proj2/run_test.sh
@@ -2,4 +2,4 @@
 
 spark-submit  --packages com.hortonworks:shc-core:1.1.1-2.1-s_2.11 --repositories http://repo.hortonworks.com/content/groups/public/ test_r.py
 
-python test_file.py
+#python test_file.py
